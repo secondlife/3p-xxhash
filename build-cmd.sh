3f287c65253a2b0bd435c03966f5fed9942b2a5e
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2023-01-30 23:14:34 +0200

    SL-19110 First configuration for xxhash

---
+++ build-cmd.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for parabuild logs.
+exec 4>&1; export BASH_XTRACEFD=4; set -x
+
+# make errors fatal
+set -e
+
+# complain about unreferenced environment variables
+set -u
+
+if [ -z "$AUTOBUILD" ] ; then
+    exit 1
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    autobuild="$(cygpath -u $AUTOBUILD)"
+else
+    autobuild="$AUTOBUILD"
+fi
+
+top="$(pwd)"
+stage="$top/stage"
+
+# load autobuild provided shell functions and variables
+source_environment_tempfile="$stage/source_environment.sh"
+"$autobuild" source_environment > "$source_environment_tempfile"
+. "$source_environment_tempfile"
+
+XXHASH_SOURCE_DIR="xxHash-release"
+
+# version will end with something like '160 /* 0.16 */'
+version="0.8.1"
+
+build=${AUTOBUILD_BUILD_ID:=0}
+echo "${version}.${build}" > "${stage}/VERSION.txt"
+
+pushd "$XXHASH_SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+
+        windows*)
+            # looks like we do not need a full build, just the massive header
+
+            mkdir -p "$stage/include/xxhash"
+            cp -a xxhash.h "$stage/include/xxhash/xxhash.h"
+        ;;
+
+        darwin*)
+            mkdir -p "$stage/include/xxhash"
+            cp -a xxhash.h "$stage/include/xxhash/xxhash.h"
+        ;;
+
+        linux*)
+            mkdir -p "$stage/include/xxhash"
+            cp -a xxhash.h "$stage/include/xxhash/xxhash.h"
+        ;;
+    esac
+    mkdir -p "$stage/LICENSES"
+    cp -a LICENSE "$stage/LICENSES/xxhash.txt"
+popd
+
+#mkdir -p "$stage"/docs/xxhash/
+#cp -a README.Linden "$stage"/docs/xxhash/
